bba5d9af7fb41faebfe42a34b1ba817e331d75ec
ghassan <ghassan1000@live.com>
2024-05-16 19:38:46 +0300

    service 1 finished

--- service1/server.js
+++ service1/server.js
@@ -2,19 +2,40 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require("mongoose");
+
+// Setup Database
+const db_url = "mongodb+srv://admin:admin@coe453project.b7gjegg.mongodb.net/?retryWrites=true&w=majority&appName=coe453project";
+mongoose.connect(db_url);
+
+// Define Food Schema
+const Food_Schema = new mongoose.Schema({
+  name: String,
+  calories: {
+      type: Number,
+      min: 0
+  }
+});
+
+// Define Food Model
+const Food = mongoose.model('Food', Food_Schema);
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
 
-  type Food {
-    name: String
-    calories: Int
-    id: String
+  type RootQuery {
+  hello: String!
   }
 
-  type Mutation {
-    getCalories(food: Food, weight: Int): Int
+  type RootMutation {
+    getCalories(foodId: String, weight: Int): Float
   }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+ }
+
 `);
 
 // The root provides a resolver function for each API endpoint
@@ -22,7 +43,10 @@ const root = {
   hello: () => {
     return 'Hello, world!';
   },
-  getCalories: ({ food, weight }) => food.calories*weight/100,
+  getCalories: async ({ foodId, weight }) => {
+    const food = await Food.findById(foodId);
+    return weight * food.calories / 100;
+  },
 };
 
 const app = express();
@@ -39,5 +63,5 @@ app.use(
 );
 
 app.listen(3000, () =>
-  console.log('Running a GraphQL API server at port 3000')
+  console.log('Running a GraphQL API server at http://localhost:3000/')
 );
